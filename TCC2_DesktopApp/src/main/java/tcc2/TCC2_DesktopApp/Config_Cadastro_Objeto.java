3303adddb2d231747f7919be39fb7c05a933973d
niltonmgarcez <niltongarcez@gmail.com>
2014-10-26 13:28:02 -0200

    Config Cadastro Objeto pronto

---
+++ TCC2_DesktopApp/src/main/java/tcc2/TCC2_DesktopApp/Config_Cadastro_Objeto.java
@@ -0,0 +1,273 @@
+package tcc2.TCC2_DesktopApp;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.LineBorder;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+
+import org.bson.BSONObject;
+import org.bson.types.BasicBSONList;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Config_Cadastro_Objeto extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	MongoClient mongoClient = new MongoClient();
+	DB db = mongoClient.getDB("TCC2_Data");
+	DBCollection predio = db.getCollection("Macroambiente");
+	DBCollection objetos = db.getCollection("Objetos");
+	BasicDBObject documento = new BasicDBObject();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Config_Cadastro_Objeto frame = new Config_Cadastro_Objeto();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Config_Cadastro_Objeto() throws UnknownHostException {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 589, 231);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		final JComboBox comboBox = new JComboBox();
+		final JComboBox comboBox_1 = new JComboBox();
+		final JTextArea textArea = new JTextArea();
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		
+		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				documento.clear();
+				String tamanhoTabela = String.valueOf(objetos.getCount() + 1);
+				documento.put("objeto_ID", tamanhoTabela);
+				documento.put("tipo de objeto", comboBox.getSelectedItem());
+				if (comboBox_1.getSelectedItem().equals("Térreo"))
+				{
+					documento.put("andar", "1");
+				}else{
+					documento.put("andar", comboBox_1.getSelectedItem().toString().substring(0, 1));
+				}
+				documento.put("nome do objeto", textField.getText());
+				documento.put("latitude", textField_1.getText());
+				documento.put("longitude", textField_2.getText());
+				documento.put("descricao", textArea.getText());
+				
+				objetos.insert(documento);
+						
+				DBCursor cursor = objetos.find();
+				while (cursor.hasNext()) {
+					System.out.println(cursor.next());
+				}
+				
+			}
+		});
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("predio_ID", "1");
+		DBCursor result = predio.find(searchQuery);
+	    Pattern pattern = Pattern.compile("\"(.*?)\"");
+		DBObject oneDetails = result.next();
+	    String data=oneDetails.get("tipos de objeto").toString();
+	    Matcher matcher = pattern.matcher(data);
+	    
+		while (matcher.find()) {  
+	        String text = matcher.group(1);
+	        comboBox.addItem(text);
+		}
+		
+		Integer andares = Integer.parseInt(oneDetails.get("andares").toString());
+		int count = 1;
+		while (count <= andares) {  
+			if (count == 1)
+			{
+				comboBox_1.addItem("Térreo");
+			}else{
+				comboBox_1.addItem(count + "º Andar");
+			}	        
+			count++;
+		}
+			
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				comboBox.setSelectedItem("");
+				comboBox_1.setSelectedItem("");	
+				textField.setText("");
+				textField_1.setText("");
+				textField_2.setText("");
+				textArea.setText("");
+			}
+		});
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Config_Cadastro_Objeto.this.dispose();
+			}
+		});
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 472, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(btnCadastrar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnLimpar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnCancelar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnCadastrar)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnLimpar)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnCancelar))
+						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		
+		JLabel lblTipoDeAmbiente = new JLabel("Tipo de Objeto");
+		
+	
+		JLabel lblAndar = new JLabel("Andar:");
+		
+		
+		JLabel lblNomeDoAmbiente = new JLabel("Nome do Obejto");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JLabel lblLatitude = new JLabel("Latitude:");
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		
+		JLabel lblLongitude = new JLabel("Longitude:");
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Descrição");
+		
+
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(lblTipoDeAmbiente)
+							.addGap(26)
+							.addComponent(comboBox, 0, 153, GroupLayout.DEFAULT_SIZE)
+							.addGap(18)
+							.addComponent(lblAndar)
+							.addGap(18)
+							.addComponent(comboBox_1, 0, 131, Short.MAX_VALUE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNomeDoAmbiente)
+								.addComponent(lblLatitude)
+								.addComponent(lblNewLabel))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 362, Short.MAX_VALUE)
+								.addGroup(gl_panel.createSequentialGroup()
+									.addComponent(textField_1, 84, 84, 84)
+									.addGap(41)
+									.addComponent(lblLongitude)
+									.addPreferredGap(ComponentPlacement.RELATED, 103, Short.MAX_VALUE)
+									.addComponent(textField_2, 83, 83, 83))
+								.addComponent(textField, GroupLayout.DEFAULT_SIZE, 362, Short.MAX_VALUE))))
+					.addContainerGap())
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblTipoDeAmbiente)
+						.addComponent(comboBox)
+						.addComponent(lblAndar)
+						.addComponent(comboBox_1))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNomeDoAmbiente)
+						.addComponent(textField))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblLatitude)
+						.addComponent(textField_1)
+						.addComponent(lblLongitude)
+						.addComponent(textField_2))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel)
+						.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
