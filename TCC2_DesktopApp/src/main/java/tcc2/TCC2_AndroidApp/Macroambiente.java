edc710aa0fc66fb36d072ca47df21b1152ae8435
niltonmgarcez <niltongarcez@gmail.com>
2014-11-01 20:31:55 -0200

    Macroambiente done!

--- TCC2_DesktopApp/src/main/java/tcc2/TCC2_AndroidApp/Macroambiente.java
+++ TCC2_DesktopApp/src/main/java/tcc2/TCC2_AndroidApp/Macroambiente.java
@@ -6,12 +6,27 @@ package tcc2.TCC2_AndroidApp;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
 /**
  * @author Nilton Mena Garcez
  *
  */
 public class Macroambiente {
 
+	String textUri = "mongodb://niltongarcez:trabalho2@ds047440.mongolab.com:47440/tcc2_data";
+	MongoClientURI uri = new MongoClientURI(textUri);
+	MongoClient mongoClient = new MongoClient(uri);
+	DB db = mongoClient.getDB("tcc2_data");
+	DBCollection predio = db.getCollection("Macroambiente");
+	BasicDBObject documento = new BasicDBObject();
+	
 	String identificacao;
 	String descricao;
 	ArrayList<Ambiente> listaAmbientes = new ArrayList<Ambiente>();
@@ -24,15 +39,13 @@ public class Macroambiente {
 	 * @param andares
 	 * @throws UnknownHostException 
 	 */
-	public Macroambiente(String identificacao, String descricao,
-			ArrayList<Ambiente> listaAmbientes, int andares) throws UnknownHostException {
-		super();
-		this.identificacao = identificacao;
-		this.descricao = descricao;
-		this.listaAmbientes = listaAmbientes;
-		this.andares = andares;
+	public Macroambiente() throws UnknownHostException {
+		DBCursor cursor = predio.find();
+		DBObject objeto = cursor.next();
+		identificacao = objeto.get("nome").toString();
+		descricao = objeto.get("descricao").toString();
 		listaAmbientes = ambientes.getListaAmbientes();
-		
+		andares = Integer.parseInt(objeto.get("andares").toString());		
 	}
 	public String getIdentificacao() {
 		return identificacao;
@@ -61,5 +74,18 @@ public class Macroambiente {
 	public Ambiente getAmbiente(String Ambiente)
 	{
 		return ambientes.getAmbiente(Ambiente);
+	}
+	
+	public static void main(String[] args) throws UnknownHostException {
+		
+		Macroambiente mamb = new Macroambiente();
+		System.out.println(mamb.getIdentificacao());
+		System.out.println(mamb.getDescricao());
+		for (int i = 0; i < mamb.getListaAmbientes().size(); i++)
+		{
+			System.out.println(mamb.getListaAmbientes().get(i).getIdentificacao().toString());
+		}
+		System.out.println(mamb.getAndares());
+
 	}
 }
